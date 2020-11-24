51368563f89816262913dae4f493cff15b80b8b5
Patrick Wong <patrick.wong@indexexchange.com>
2020-11-24 15:25:05 -0500

    Things happened

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,38 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TeamLeaderboardComponent } from './team-leaderboard/team-leaderboard.component';
+import { IndividualLeaderboardComponent } from './individual-leaderboard/individual-leaderboard.component';
+import { TeamPanelComponent } from './team-panel/team-panel.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { MatCardModule } from '@angular/material/card';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+import { LeaderboardApiService } from './services/leaderboard-api.service';
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TeamLeaderboardComponent,
+    IndividualLeaderboardComponent,
+    TeamPanelComponent,
+    LandingPageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatCardModule,
+    MatGridListModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    MatListModule,
+    BrowserAnimationsModule
   ],
-  providers: [],
+  providers: [
+    LeaderboardApiService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
