cb8351128d0713ed02c4eaf03c95cf84ca1eef58
Emma Wilson <emma.wilson@indexexchange.com>
2020-11-24 16:11:16 -0500

    add procfile

--- server.js
+++ server.js
@@ -7,9 +7,9 @@ const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/leaderboard-ui'));
 
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/leaderboard-ui/index.html'));
+app.get('/*', function (req, res) {
+
+    res.sendFile(path.join(__dirname + '/dist/leaderboard-ui/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
