0a6a7f527cda07edb23f56bfc186824b34ed1643
Emma Wilson <emma.wilson@indexexchange.com>
2020-11-27 16:34:23 -0500

    staging

--- src/app/env.ts
+++ src/app/env.ts
@@ -1,2 +1,2 @@
 // export const API_URL = 'http://localhost:5000';
-export const API_URL = 'https://team-snitch-hackathon.herokuapp.com/';
+export const API_URL = 'https://team-snitch-hackathon-staging.herokuapp.com/';
